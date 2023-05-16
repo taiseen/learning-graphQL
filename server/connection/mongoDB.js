18c63146f1b96d55024d4c456cf0bb19c38bd0ba
taiseen <taiseen.cse@gmail.com>
2023-05-16 18:19:17 +0600

    refactor: async/await add at db connect function

--- server/connection/mongoDB.js
+++ server/connection/mongoDB.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-const mongoDB = () => {
+const mongoDB = async () => {
 
     try {
         // Remove console warning...
         mongoose.set('strictQuery', true);
-        mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
         console.log("Connected To MongoDB ==> OK ✅");
     } catch (error) {
